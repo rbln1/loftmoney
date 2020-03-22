af405f09e1ddb8cf7ca45a3b7ee55c81c9e6e325
Andrey Dolgushin <mailer.rand@gmail.com>
2020-03-22 19:50:06 +0500

    The first task is completed

--- app/src/main/java/me/rubl/loftmoney/MainActivity.java
+++ app/src/main/java/me/rubl/loftmoney/MainActivity.java
@@ -1,15 +1,25 @@
 package me.rubl.loftmoney;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mAddItemBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAddItemBtn = findViewById(R.id.add_item_btn);
+
+        mAddItemBtn.setOnClickListener((v) -> {
+            startActivity(new Intent(getApplicationContext(), AddItemActivity.class));
+        });
 
     }
 }
